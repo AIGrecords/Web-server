bfdf5e5f9e416f014167eb0881f23092dd971dbb
AIG(ムIG) <58285388+AIGrecords@users.noreply.github.com>
2021-03-01 18:04:41 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,13 @@
 const http = require('http')
 const port = 3000
 
-const server = http.createServer()
+const server = http.createServer(function(req, res) {
+
+})
+
+server.listen(port, function(error) {
+  if (error) {
+    console.log("Something went wrong")}
+  else {
+    console.log("Server is listening on port ", + port)}
+})
